b71d1cde3027b893d1c01c83f7828886fcfdd75b
B DURGA PRASAD <durga3887@gmail.com>
2024-06-24 15:47:33 +0530

    Create Pointers.c
    Initial program

---
+++ Pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *fun(int n, int** address)   // n is the no.of memory blocks needed , Address is the double pointer to get 
+{
+    *address = (int*)malloc(3*sizeof(int));
+    return *address;
+}
+
+
+int main()
+{
+    int *ptr = NULL, *ptr1 = NULL;
+    ptr=fun(3,&ptr1);
+    
+    printf("ptr1:%p\nptr:%p",ptr1,ptr);
+
+    return 0;
+}
