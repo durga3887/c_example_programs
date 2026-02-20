ef8eb8ce783a17123d6aec31dcf6e35c5babc327
B DURGA PRASAD <durga3887@gmail.com>
2026-02-20 06:38:40 +0530

    Update SWAP_array.c

--- SWAP_array.c
+++ SWAP_array.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int count = 0; 
+    while((*(a+count) !='\0') || (*(b+count) != '\0'))
+    {
+        if(*(a+count) != '\0' )
+        {
+            if(*(b+count) != '\0' )
+            {
+                *(a\+count) = *(a+count) + *(b+count);
+                *(b+count) = *(a+count) - *(b+count);
+                *(a+count) = *(a+count) - *(b+count);
+                
+            }
+            else
+            {
+                
+            }
+        }
+        if(*(b+count) != '\0' )
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    return *address;
+
+    return 0;
+}
